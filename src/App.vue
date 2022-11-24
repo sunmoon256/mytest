66e26749f5353cc141165f3b3bfed1c5edf3a14e
sunmoon256 <sunmoon06142@gmail.com>
2022-11-24 09:58:22 +0800

    项目环境搭建

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+#app {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/common/Mapview.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="mapview"></div>
+  </template>
+  
+  <script>
+  export default {
+    // name: '',
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style>
+  .mapview{
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+  </style>
+  
